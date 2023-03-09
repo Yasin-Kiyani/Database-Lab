d9b195ac9064aa07084b72e1d8d4bf255143c85e
Yasin Kiyani | یاسین کیانی | Developer <82681889+Yasin-Kiyani@users.noreply.github.com>
2023-03-09 20:31:03 +0330

    update

--- Tamrin2/Soft Delete اضافه شدن در انتهای کد های قبلی/کد های دو جدول باهم.sql
+++ Tamrin2/Soft Delete اضافه شدن در انتهای کد های قبلی/کد های دو جدول باهم.sql
@@ -1,36 +1,16 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 03, 2023 at 12:05 AM
--- Server version: 8.0.27
--- PHP Version: 8.0.13
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+--> Database: `kiyanpcshop`
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `kiyanpcshop`
---
-CREATE DATABASE IF NOT EXISTS `kiyanpcshop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_persian_ci;
+CREATE DATABASE  `kiyanpcshop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_persian_ci;
 USE `kiyanpcshop`;
 
--- --------------------------------------------------------
+-----------------------------------------------------
 
---
--- Table structure for table `contactus`
---
 
-DROP TABLE IF EXISTS `contactus`;
-CREATE TABLE IF NOT EXISTS `contactus` (
+--> Table structure for table `contactus`
+
+
+CREATE TABLE  `contactus` (
   `id` int NOT NULL AUTO_INCREMENT,
   `fname` varchar(50) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
   `lname` varchar(50) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
@@ -44,22 +24,55 @@ CREATE TABLE IF NOT EXISTS `contactus` (
   UNIQUE KEY `tel` (`tel`)
 ) ENGINE=MyISAM AUTO_INCREMENT=83 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_persian_ci;
 
---
--- Dumping data for table `contactus`
---
+--------------------------------------------------
+
+--> insert data for table `contactus`
+
 
 INSERT INTO `contactus` (`id`, `fname`, `lname`, `email`, `tel`, `title`, `textbody`) VALUES
-(1, 'Yasin', 'Kiyani', 'YasinKiyani@gmail.com', '09918654559', 'کمکم کنید ', 'یک مشکلی برای خرید پیش امده', '1'),
-(2, 'Ali', 'Kiyani', 'AliKiyani@gmail.com', '09303818172', 'تشکر از سایت', 'سایت بسیار مشکل گشا و درجه یکی دارید', '0');
+(1, 'Yasin', 'Kiyani', 'YasinKiyani@gmail.com', '09918654559', 'کمکم کنید ', 'یک مشکلی برای خرید پیش امده'),
+(2, 'Ali', 'Kiyani', 'AliKiyani@gmail.com', '09303818172', 'تشکر از سایت', 'سایت بسیار مشکل گشا و درجه یکی دارید');
+
+
+-----------------------------------------------------
+
+--> SOFT DELETE
+0 = status = not received
+1 = status = received
+
+-->  Delete data of table `contactus` with enum
+
+DELETE FROM contactus WHERE `contactus`.`status` = '1'
+DELETE FROM contactus WHERE `contactus`.`status` = '0'
+
+-->  Select data of table `contactus`
+
+SELECT * FROM `user` WHERE status='1'
+
+-->  Update data of table `contactus`
+
+UPDATE contactus
+SET status = '1'
+WHERE status = 0;
+
+-->  2Update data of table `contactus`
+
+UPDATE `contactus` SET `not_recive`='recive' WHERE `id`=1
+
+
+
+-----------------------------------------------------
+
+-->  Select data of table `contactus`
+
+SELECT * FROM `contactus` WHERE id=1
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `user`
---
+--> Table structure for table `user`
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
+
+CREATE TABLE  `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `Rolls` varchar(255) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
   `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_persian_ci NOT NULL,
@@ -80,15 +93,44 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `password` (`password`)
 ) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_persian_ci;
 
---
--- Dumping data for table `user`
---
+
+-----------------------------------------------------
+
+
+--> insert data for table `user`
 
 INSERT INTO `user` (`id`, `Rolls`, `username`, `email`, `password`, `Favorites1`, `Favorites2`, `Favorites3`, `gender`, `Datebirth`, `cities`, `file_name`, `uploaded_on`, `status`) VALUES
 (1, 'admin', 'admin', 'admin@gmail.com', '@Admin1234', 'افزونه های وردپرسی', 'قالب های وردپرس', 'دروره های آموزشی طرحی سایت', 'مرد', '2000-01-17', 'اصفهان', 'AdminPhotoProfile.png', '2023-01-01 00:06:58', '1'),
 (2, 'Nuser', 'user', 'user@gmail.com', '@User1234', 'افزونه های وردپرسی', 'قالب های وردپرسی', 'سایت های وردپرسی', 'مرد', '2000-01-17', 'اصفهان', 'UserPhotoProfile.png', '2023-01-01 00:06:58', '1');
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-----------------------------------------------------
+
+
+
+--> SOFT DELETE
+0 = status = Diactive
+1 = status = Active
+
+-->  Delete data of table `user` with enum
+
+DELETE FROM contactus WHERE `contactus`.`status` = '1'
+DELETE FROM contactus WHERE `contactus`.`status` = '0'
+
+-->  Select data of table `user`
+
+SELECT * FROM `user` WHERE status='1'
+
+-->  Update data of table `user`
+
+UPDATE user
+SET status = '1'
+WHERE status = 0;
+
+-->  2Update data of table `user`
+
+UPDATE `user` SET `is_active`='deactive' WHERE `id`=1
+
+-----------------------------------------------------
+
